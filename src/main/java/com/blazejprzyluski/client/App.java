87cab8bb3880986a3be4bf01db4ff743461fa09e
Blazej Przyluski <blazejprzyl@gmail.com>
2019-07-24 20:40:10 +0200

    First update in new branch

---
+++ src/main/java/com/blazejprzyluski/client/App.java
@@ -0,0 +1,12 @@
+package com.blazejprzyluski.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class App implements EntryPoint
+{
+    @Override
+    public void onModuleLoad()
+    {
+
+    }
+}
